fc5fb36477d3b21364ddc3d99063f0a316959eef
Piotrek <piotrekwitkowski@users.noreply.github.com>
2026-04-24 20:27:46 +0100

    Bump devDependencies to latest majors
    Add test/tsconfig.json with explicit jest types and point ts-jest at it,
    since @types/jest v30 no longer auto-injects globals.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'test/tsconfig.json' }],
   },
   testMatch: ['**/test/**/*.test.ts'],
 };
